d6fc0136dd841db8814a6597e4a83fba6e7b910f
saakshiece-lgtm <saakshi.ece@gmail.com>
2026-08-30 21:07:09 +0530

    2:1 MULTIPLEXER

--- design.v
+++ design.v
@@ -7,46 +7,7 @@ module mux2to1(
     output Y
 );
 
-    assign Y = S ? I1 : I0;
-
-endmodule
-
-
-
-
-module mux4to1(
-    input I0,
-    input I1,
-    input I2,
-    input I3,
-    input S1,
-    input S0,
-    output Y
-);
-
-    wire w0;
-    wire w1;
-
     
-    mux2to1 M0(
-        .I0(I0),
-        .I1(I1),
-        .S(S0),
-        .Y(w0)
-    );
-
-    mux2to1 M1(
-        .I0(I2),
-        .I1(I3),
-        .S(S0),
-        .Y(w1)
-    );
-
-    mux2to1 M2(
-        .I0(w0),
-        .I1(w1),
-        .S(S1),
-        .Y(Y)
-    );
+    assign Y = S ? I1 : I0;
 
 endmodule
--- tb.v
+++ tb.v
@@ -1,62 +1,55 @@
 `timescale 1ns/1ps
 
-module tb_mux4to1;
+module tb_mux2to1;
 
     reg I0;
     reg I1;
-    reg I2;
-    reg I3;
-    reg S1;
-    reg S0;
+    reg S;
 
     wire Y;
 
     
-    mux4to1 DUT(
+    mux2to1 DUT(
         .I0(I0),
         .I1(I1),
-        .I2(I2),
-        .I3(I3),
-        .S1(S1),
-        .S0(S0),
+        .S(S),
         .Y(Y)
     );
 
     initial begin
 
         
-        $dumpfile("mux4to1.vcd");
-        $dumpvars(0, tb_mux4to1);
+        $dumpfile("mux2to1.vcd");
+        $dumpvars(0, tb_mux2to1);
 
         
-        $display("Time\tI0 I1 I2 I3\tS1 S0\tY");
-        $monitor("%0t\t%b  %b  %b  %b\t %b  %b\t%b",
-                 $time, I0, I1, I2, I3, S1, S0, Y);
+        $display("Time\tI0\tI1\tS\tY");
+        $monitor("%0t\t%b\t%b\t%b\t%b",
+                 $time, I0, I1, S, Y);
 
         
-        I0 = 1'b0;
-        I1 = 1'b1;
-        I2 = 1'b0;
-        I3 = 1'b1;
-
-        
-        S1 = 1'b0;
-        S0 = 1'b0;
+        I0 = 1'b0; I1 = 1'b0; S = 1'b0;
         #10;
 
-        
-        S1 = 1'b0;
-        S0 = 1'b1;
+        I0 = 1'b0; I1 = 1'b1; S = 1'b0;
         #10;
 
-        
-        S1 = 1'b1;
-        S0 = 1'b0;
+        I0 = 1'b1; I1 = 1'b0; S = 1'b0;
         #10;
 
-        
-        S1 = 1'b1;
-        S0 = 1'b1;
+        I0 = 1'b1; I1 = 1'b1; S = 1'b0;
+        #10;
+
+        I0 = 1'b0; I1 = 1'b0; S = 1'b1;
+        #10;
+
+        I0 = 1'b0; I1 = 1'b1; S = 1'b1;
+        #10;
+
+        I0 = 1'b1; I1 = 1'b0; S = 1'b1;
+        #10;
+
+        I0 = 1'b1; I1 = 1'b1; S = 1'b1;
         #10;
 
         $finish;
