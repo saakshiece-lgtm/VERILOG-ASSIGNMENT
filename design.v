6d0463060c5cb72b4b8c97cd2a5d23e42198a463
saakshiece-lgtm <saakshi.ece@gmail.com>
2026-08-30 21:06:35 +0530

    4:1 MULTIPLEXER

--- design.v
+++ design.v
@@ -1,14 +1,52 @@
 
 
-module half_subtractor(
-    input A,
-    input B,
-    output Diff,
-    output Borrow
+module mux2to1(
+    input I0,
+    input I1,
+    input S,
+    output Y
 );
 
+    assign Y = S ? I1 : I0;
+
+endmodule
+
+
+
+
+module mux4to1(
+    input I0,
+    input I1,
+    input I2,
+    input I3,
+    input S1,
+    input S0,
+    output Y
+);
+
+    wire w0;
+    wire w1;
+
     
-    xor (Diff, A, B);
-    and (Borrow, B, ~A);
+    mux2to1 M0(
+        .I0(I0),
+        .I1(I1),
+        .S(S0),
+        .Y(w0)
+    );
+
+    mux2to1 M1(
+        .I0(I2),
+        .I1(I3),
+        .S(S0),
+        .Y(w1)
+    );
+
+    mux2to1 M2(
+        .I0(w0),
+        .I1(w1),
+        .S(S1),
+        .Y(Y)
+    );
 
 endmodule
--- tb.v
+++ tb.v
@@ -1,48 +1,62 @@
 `timescale 1ns/1ps
 
-module tb_half_subtractor;
+module tb_mux4to1;
 
-    reg A;
-    reg B;
+    reg I0;
+    reg I1;
+    reg I2;
+    reg I3;
+    reg S1;
+    reg S0;
 
-    wire Diff;
-    wire Borrow;
+    wire Y;
 
     
-    half_subtractor DUT(
-        .A(A),
-        .B(B),
-        .Diff(Diff),
-        .Borrow(Borrow)
+    mux4to1 DUT(
+        .I0(I0),
+        .I1(I1),
+        .I2(I2),
+        .I3(I3),
+        .S1(S1),
+        .S0(S0),
+        .Y(Y)
     );
 
     initial begin
 
         
-        $dumpfile("half_subtractor.vcd");
-        $dumpvars(0, tb_half_subtractor);
+        $dumpfile("mux4to1.vcd");
+        $dumpvars(0, tb_mux4to1);
 
         
-        $display("Time\tA\tB\tDiff\tBorrow");
-        $monitor("%0t\t%b\t%b\t%b\t%b",
-                 $time, A, B, Diff, Borrow);
+        $display("Time\tI0 I1 I2 I3\tS1 S0\tY");
+        $monitor("%0t\t%b  %b  %b  %b\t %b  %b\t%b",
+                 $time, I0, I1, I2, I3, S1, S0, Y);
 
         
+        I0 = 1'b0;
+        I1 = 1'b1;
+        I2 = 1'b0;
+        I3 = 1'b1;
 
-        A = 1'b0;
-        B = 1'b0;
+        
+        S1 = 1'b0;
+        S0 = 1'b0;
         #10;
 
-        A = 1'b0;
-        B = 1'b1;
+        
+        S1 = 1'b0;
+        S0 = 1'b1;
         #10;
 
-        A = 1'b1;
-        B = 1'b0;
+        
+        S1 = 1'b1;
+        S0 = 1'b0;
         #10;
 
-        A = 1'b1;
-        B = 1'b1;
+        
+        S1 = 1'b1;
+        S0 = 1'b1;
         #10;
 
         $finish;
