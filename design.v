5a62ce2509918933b028dd808b94aad7a27ad18a
saakshiece-lgtm <saakshi.ece@gmail.com>
2026-08-30 21:11:30 +0530

    4 BIT MAGNITUDE COMPARATOR

--- design.v
+++ design.v
@@ -1,20 +1,37 @@
 
 
-module comparator1bit(
-    input A,
-    input B,
-    output A_gt_B,
-    output A_eq_B,
-    output A_lt_B
+module comparator4bit(
+    input  [3:0] A,
+    input  [3:0] B,
+    output A_greater,
+    output A_equal,
+    output A_less
 );
 
     
-    assign A_gt_B = A & ~B;
+    wire e3, e2, e1, e0;
+
+    
+    assign e3 = ~(A[3] ^ B[3]);
+    assign e2 = ~(A[2] ^ B[2]);
+    assign e1 = ~(A[1] ^ B[1]);
+    assign e0 = ~(A[0] ^ B[0]);
+
+    
+    assign A_greater =
+        ( A[3] & ~B[3] ) |
+        ( e3 & A[2] & ~B[2] ) |
+        ( e3 & e2 & A[1] & ~B[1] ) |
+        ( e3 & e2 & e1 & A[0] & ~B[0] );
 
 
-    assign A_eq_B = ~(A ^ B);
+    assign A_equal = e3 & e2 & e1 & e0;
 
-
-    assign A_lt_B = ~A & B;
+    
+    assign A_less =
+        ( ~A[3] & B[3] ) |
+        ( e3 & ~A[2] & B[2] ) |
+        ( e3 & e2 & ~A[1] & B[1] ) |
+        ( e3 & e2 & e1 & ~A[0] & B[0] );
 
 endmodule
--- tb.v
+++ tb.v
@@ -1,52 +1,56 @@
 `timescale 1ns/1ps
 
-module tb_comparator1bit;
+module tb_comparator4bit;
 
-    reg A;
-    reg B;
+    reg [3:0] A;
+    reg [3:0] B;
 
-    wire A_gt_B;
-    wire A_eq_B;
-    wire A_lt_B;
+    wire A_greater;
+    wire A_equal;
+    wire A_less;
 
     
-    comparator1bit DUT(
+    comparator4bit DUT(
         .A(A),
         .B(B),
-        .A_gt_B(A_gt_B),
-        .A_eq_B(A_eq_B),
-        .A_lt_B(A_lt_B)
+        .A_greater(A_greater),
+        .A_equal(A_equal),
+        .A_less(A_less)
     );
 
     initial begin
 
         
-        $dumpfile("comparator1bit.vcd");
-        $dumpvars(0, tb_comparator1bit);
+        $dumpfile("comparator4bit.vcd");
+        $dumpvars(0, tb_comparator4bit);
 
         
-        $display("Time\tA\tB\tA_gt_B\tA_eq_B\tA_lt_B");
+        $display("Time\tA\tB\tA>B\tA=B\tA<B");
         $monitor("%0t\t%b\t%b\t%b\t%b\t%b",
-                 $time, A, B, A_gt_B, A_eq_B, A_lt_B);
+                 $time, A, B, A_greater, A_equal, A_less);
 
         
-        A = 1'b0;
-        B = 1'b0;
-        #10;
-
-    
-        A = 1'b0;
-        B = 1'b1;
+        A = 4'b0101;
+        B = 4'b0101;
         #10;
 
         
-        A = 1'b1;
-        B = 1'b0;
+        A = 4'b1010;
+        B = 4'b0110;
         #10;
 
         
-        A = 1'b1;
-        B = 1'b1;
+        A = 4'b0011;
+        B = 4'b1100;
+        #10;
+
+        
+        A = 4'b1111;
+        B = 4'b0000;
+        #10;
+
+        A = 4'b0000;
+        B = 4'b1111;
         #10;
 
         $finish;
