a223905c4d3d2578d0b71a12130e30271484c1ff
saakshiece-lgtm <saakshi.ece@gmail.com>
2026-08-30 21:08:09 +0530

    BCD 7 SEGMENT DISPLAY

--- design.v
+++ design.v
@@ -1,14 +1,61 @@
 
 
-module half_adder(
-    input A,
-    input B,
-    output Sum,
-    output Carry
+module bcd_to_7segment(
+    input  [3:0] bcd,
+    output a,
+    output b,
+    output c,
+    output d,
+    output e,
+    output f,
+    output g
 );
 
     
-    xor (Sum, A, B);
-    and (Carry, A, B);
+    assign a = (~bcd[3] & ~bcd[2] & ~bcd[1] &  bcd[0]) |
+               (~bcd[3] &  bcd[2] & ~bcd[1] & ~bcd[0]) |
+               ( bcd[3] & ~bcd[2] &  bcd[1] &  bcd[0]) |
+               ( bcd[3] &  bcd[2] & ~bcd[1] &  bcd[0]);
+
+    
+    assign b = (~bcd[3] &  bcd[2] & ~bcd[1] &  bcd[0]) |
+               (~bcd[3] &  bcd[2] &  bcd[1] & ~bcd[0]) |
+               ( bcd[3] & ~bcd[2] &  bcd[1] &  bcd[0]) |
+               ( bcd[3] &  bcd[2] & ~bcd[1] & ~bcd[0]) |
+               ( bcd[3] &  bcd[2] &  bcd[1] &  bcd[0]);
+
+    
+    assign c = (~bcd[3] & ~bcd[2] &  bcd[1] & ~bcd[0]) |
+               ( bcd[3] &  bcd[2] & ~bcd[1] & ~bcd[0]) |
+               ( bcd[3] &  bcd[2] &  bcd[1] & ~bcd[0]) |
+               ( bcd[3] &  bcd[2] &  bcd[1] &  bcd[0]);
+
+    
+    assign d = (~bcd[3] & ~bcd[2] & ~bcd[1] &  bcd[0]) |
+               (~bcd[3] &  bcd[2] & ~bcd[1] & ~bcd[0]) |
+               (~bcd[3] &  bcd[2] &  bcd[1] &  bcd[0]) |
+               ( bcd[3] & ~bcd[2] & ~bcd[1] &  bcd[0]) |
+               ( bcd[3] & ~bcd[2] &  bcd[1] & ~bcd[0]);
+
+    
+    assign e = (~bcd[3] & ~bcd[2] & ~bcd[1] &  bcd[0]) |
+               (~bcd[3] & ~bcd[2] &  bcd[1] &  bcd[0]) |
+               (~bcd[3] &  bcd[2] & ~bcd[1] & ~bcd[0]) |
+               (~bcd[3] &  bcd[2] & ~bcd[1] &  bcd[0]) |
+               (~bcd[3] &  bcd[2] &  bcd[1] &  bcd[0]) |
+               ( bcd[3] & ~bcd[2] & ~bcd[1] &  bcd[0]);
+
+    
+    assign f = (~bcd[3] & ~bcd[2] & ~bcd[1] &  bcd[0]) |
+               (~bcd[3] & ~bcd[2] &  bcd[1] & ~bcd[0]) |
+               (~bcd[3] & ~bcd[2] &  bcd[1] &  bcd[0]) |
+               (~bcd[3] &  bcd[2] &  bcd[1] &  bcd[0]) |
+               ( bcd[3] & ~bcd[2] & ~bcd[1] &  bcd[0]);
+
+    
+    assign g = (~bcd[3] & ~bcd[2] & ~bcd[1] & ~bcd[0]) |
+               (~bcd[3] & ~bcd[2] & ~bcd[1] &  bcd[0]) |
+               (~bcd[3] &  bcd[2] &  bcd[1] &  bcd[0]) |
+               ( bcd[3] & ~bcd[2] & ~bcd[1] &  bcd[0]);
 
 endmodule
--- tb.v
+++ tb.v
@@ -1,47 +1,51 @@
 `timescale 1ns/1ps
 
-module tb_half_adder;
+module tb_bcd_to_7segment;
 
-    reg A;
-    reg B;
+    reg [3:0] bcd;
 
-    wire Sum;
-    wire Carry;
+    wire a;
+    wire b;
+    wire c;
+    wire d;
+    wire e;
+    wire f;
+    wire g;
 
-    half_adder DUT(
-        .A(A),
-        .B(B),
-        .Sum(Sum),
-        .Carry(Carry)
+    
+    bcd_to_7segment DUT(
+        .bcd(bcd),
+        .a(a),
+        .b(b),
+        .c(c),
+        .d(d),
+        .e(e),
+        .f(f),
+        .g(g)
     );
 
     initial begin
 
         
-        $dumpfile("half_adder.vcd");
-        $dumpvars(0, tb_half_adder);
+        $dumpfile("bcd_to_7segment.vcd");
+        $dumpvars(0, tb_bcd_to_7segment);
 
         
-        $display("Time\tA\tB\tSum\tCarry");
-        $monitor("%0t\t%b\t%b\t%b\t%b",
-                 $time, A, B, Sum, Carry);
+        $display("Time\tBCD\tabcdefg");
+        $monitor("%0t\t%b\t%b%b%b%b%b%b%b",
+                 $time, bcd, a, b, c, d, e, f, g);
 
-        
-        A = 1'b0;
-        B = 1'b0;
-        #10;
 
-        A = 1'b0;
-        B = 1'b1;
-        #10;
-
-        A = 1'b1;
-        B = 1'b0;
-        #10;
-
-        A = 1'b1;
-        B = 1'b1;
-        #10;
+        bcd = 4'b0000; #10;  // 0
+        bcd = 4'b0001; #10;  // 1
+        bcd = 4'b0010; #10;  // 2
+        bcd = 4'b0011; #10;  // 3
+        bcd = 4'b0100; #10;  // 4
+        bcd = 4'b0101; #10;  // 5
+        bcd = 4'b0110; #10;  // 6
+        bcd = 4'b0111; #10;  // 7
+        bcd = 4'b1000; #10;  // 8
+        bcd = 4'b1001; #10;  // 9
 
         $finish;
 
